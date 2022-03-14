79f37508c0107e4b191bd5920f8dc0b6c00a0e1a
Vitoleander <89918725+Vitoleander@users.noreply.github.com>
2022-03-14 22:11:07 +0100

    reducers and servise

---
+++ src/_reducers/authentication.reducer.js
@@ -0,0 +1,27 @@
+import userEvent from '@testing-library/user-event';
+import { userConstants } from '../_constants';
+
+let user = JSON.parse(localStorage.getItem('user'));
+const initialState =  user ? { loggedIn: true, user } : {};
+
+// kollar om användaren är inloggad/utloggad
+export function authentication(state = initialState, action) {
+    switch (action.type) {
+        case userConstants.LOGIN_REQUEST:
+        return {
+            loggingIn: true,
+            user: action.user
+        };
+        case userConstants.LOGIN_SUCCESS:
+        return {
+            loggedIn: true,
+            user: action.user
+        };
+        case userConstants.LOGIN_FAILURE:
+            return {};
+        case userConstants.LOGOUT:
+            return {};
+        default:
+            return state
+    }
+}
